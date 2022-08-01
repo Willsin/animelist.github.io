917c50c8012c51c29f7ea03f5a08ec04b937f3e0
anime-list <110227160+anime-list@users.noreply.github.com>
2022-08-01 11:52:57 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -28,9 +28,10 @@ function cellSwipe() {
   document.querySelectorAll('div.ani-cell').forEach(ele => {
     ele.onpointerdown = event_down => {
       ele.setPointerCapture(event_down.pointerId)
+      let startX = event_down.clientX - ele.getBoundingClientRect().left
 
       ele.onpointermove = event_move => {
-        ele.style.left = event_move.x - event_down.y + 'px'
+        ele.style.left = event_down.clientX - ele.getBoundingClientRect().left - startX + 'px'
       }
 
       ele.onpointerup = () => {
